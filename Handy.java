c0b237fab36b2409e9557fcf636feda224911f67
khushbupathe <k.pathe96@gmail.com>
2018-06-05 12:37:29 +0530

    Add files via upload

---
+++ Handy.java
@@ -0,0 +1,56 @@
+package Hand.gesture;
+
+// Handy.java
+
+/* Detect the user's gloved hand and fingers, drawing information
+ on top of a webcam image.
+
+ Usage:
+ > run Handy
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+import com.googlecode.javacv.*;
+import com.googlecode.javacv.cpp.*;
+import com.googlecode.javacpp.Loader;
+
+public class Handy extends JFrame {
+    // GUI components
+
+    private HandPanel handPanel;
+
+    public Handy() {
+        super("Hand Gesture");
+
+        Container c = getContentPane();
+        c.setLayout(new BorderLayout());
+
+        // Preload the opencv_objdetect module to work around a known bug.
+        Loader.load(opencv_objdetect.class);
+        if (com.permission.Display.diplaay()) {
+            handPanel = new HandPanel(); // the webcam pictures and drums appear here
+            c.add(handPanel, BorderLayout.CENTER);
+        }
+
+
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                handPanel.closeDown();    // stop snapping pics, and any drum playing
+                System.exit(0);
+            }
+        });
+
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    } // end of Handy()
+
+    // -------------------------------------------------------
+    public static void main(String args[]) {
+        new Handy();
+    }
+} // end of Handy class
---
+++ MotionGueture.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hand.gesture;
+
+/**
+ *
+ * @author khushbu
+ */
+public class MotionGueture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Home().setVisible(true);
+    }
+}
